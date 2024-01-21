6f295d7f488c47a062dca620054cb740ed34f97d
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-01-20 21:25:01 -0800

    Subject: Created a Base Globe Progress Bar Blueprints
    Made a child blueprint to the AuraUserWidget Class. It will serve as both the parent to the Mana and Health Progress Bar globes.
    
    It is a very modular progress bar with Variables for Image Textures and sizing so that it can be used multiple times.

--- Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -3,3 +3,8 @@
 
 #include "UI/Widgets/AuraUserWidget.h"
 
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -6,6 +6,9 @@
 #include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
+class UAttributeSet;
+class UAbilitySystemComponent;
+
 /**
  * 
  */
@@ -13,5 +16,23 @@ UCLASS()
 class AURA_API UAuraWidgetController : public UObject
 {
 	GENERATED_BODY()
-	
+
+protected:
+
+	/*These four Variables are the most important for the widget controller
+	 * and are the ones that will be used the most. The rest are just for
+	 * convenience.
+	 */
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
