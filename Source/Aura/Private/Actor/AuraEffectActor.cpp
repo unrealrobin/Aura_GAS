e4d0be6a9b3be4213a054004316a423c7e1edccd
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-01-25 19:33:38 -0800

    Created the Boilerplate for applying and removing game effects based on the enum EffectApplicationPolicy and EffectRemovalPolicy. These Enum types will decide when an effect is applied or removed

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -61,4 +61,12 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 
 }
 
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+}
+
 
