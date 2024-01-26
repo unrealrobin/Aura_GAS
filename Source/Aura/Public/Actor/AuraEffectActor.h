1a449b83e0c7a757dd8ceaeeffcc428387819db3
Robin Lifshitz <Rlifshitz19@gmail.com>
2024-01-25 17:31:31 -0800

    Subject: Created Health Crystal HOT item.
    Created the BP for the static mesh and the collision spehere.
    Create the GE for gameplay effect data.
    Crystal Increases health by 100% for 2 seconds.

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -27,10 +27,13 @@ protected:
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
 private:
 	
 
-public:	
+
 	
 
 };
